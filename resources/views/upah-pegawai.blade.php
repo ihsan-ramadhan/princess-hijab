c1fb7e54b0ca75a1cb23290f512f5b370711eeb6
aisyahksy24-create <aisyah.ksy24@polban.ac.id>
2026-06-07 13:14:49 +0700

    Update upah-pegawai.blade.php

--- resources/views/upah-pegawai.blade.php
+++ resources/views/upah-pegawai.blade.php
@@ -87,11 +87,11 @@
             <a href="/upah-pegawai" class="nav-item nav-love active"><img src="{{ asset('images/tangan-love.svg') }}"></a>
             <a href="/pendataan" class="nav-item nav-catat"><img src="{{ asset('images/catatan-hitam.svg') }}"></a>
 =======
-            <a href="/dashboard-admin" class="nav-item nav-rumah"><img src="{{ asset('Images/rumah.svg') }}"></a>
-            <a href="/rekap-omset" class="nav-item nav-uang"><img src="{{ asset('Images/uang hitam.svg') }}"></a>
-            <a href="/pengeluaran" class="nav-item"><img src="{{ asset('Images/dompet hitam.svg') }}"></a>
-            <a href="/upah-pegawai" class="nav-item nav-love active"><img src="{{ asset('Images/tangan love.svg') }}"></a>
-            <a href="/pendataan" class="nav-item nav-catat"><img src="{{ asset('Images/catatan hitam.svg') }}"></a>
+            <a href="/dashboard-admin" class="nav-item nav-rumah"><img src="{{ asset('images/rumah.svg') }}"></a>
+            <a href="/rekap-omset" class="nav-item nav-uang"><img src="{{ asset('images/uang hitam.svg') }}"></a>
+            <a href="/pengeluaran" class="nav-item"><img src="{{ asset('images/dompet-hitam.svg') }}"></a>
+            <a href="/upah-pegawai" class="nav-item nav-love active"><img src="{{ asset('images/tangan-love.svg') }}"></a>
+            <a href="/pendataan" class="nav-item nav-catat"><img src="{{ asset('images/catatan-hitam.svg') }}"></a>
 >>>>>>> Stashed changes
         </div>
     </div>
